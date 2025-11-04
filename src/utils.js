de0cc31c847d9c28893d7916cc49680d38b0ea1e
Kollyns12 <collinsmoses295@gmail.com>
2025-11-05 00:48:32 +0300

    adds my modified files

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,11 @@
-// Your code here
+export function pointsForWord(word) {
+  let points = 0;
+
+  for (const char of word) {
+    // Case-insensitive check for vowels
+    points += /[aeiou]/i.test(char) ? 1 : 2;
+  }
+
+  return points;
+}
+
